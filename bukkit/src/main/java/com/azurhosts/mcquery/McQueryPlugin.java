c28888363e6ac71d52bca8652a00d759a85d4f7b
RedSavant <jjumper999@gmail.com>
2026-05-16 21:20:13 +0200

    feat(add): add bukkit plugin module
    - add a module to handle bukkit api
    - add plugin.yml

---
+++ bukkit/src/main/java/com/azurhosts/mcquery/McQueryPlugin.java
@@ -0,0 +1,24 @@
+package com.azurhosts.mcquery;
+
+/**
+ * MCQuery - Classe principale du plugin avec compatibilité Bukkit
+ * Mainteneur : RedSavant, OxiWan (contact@azurhosts.com)
+ * Auteur(s)   : RedSavant (rscomeback@outlook.fr)
+ * Distribué sous GNU General Public License v3.0
+ * Voir LICENSE, CONTRIBUTING.md pour plus de détails.
+ * NOTICE (GPL v3 Section 7b) : L'attribution au mainteneur doit être conservée dans toute redistribution.
+ *
+ **/
+
+public class McQueryPlugin extends Bootstrap {
+
+    @Override
+    protected void onStart() {
+
+    }
+
+    @Override
+    protected void onStop() {
+
+    }
+}
