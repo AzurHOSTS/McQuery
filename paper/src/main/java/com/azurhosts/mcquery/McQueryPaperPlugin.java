1449d33f64ff14c9dec85900cd5a8b9eec3cba77
RedSavant <jjumper999@gmail.com>
2026-05-18 20:20:07 +0200

    refactor(license): add licence mark
    - add licensmark in paper module main

--- paper/src/main/java/com/azurhosts/mcquery/McQueryPaperPlugin.java
+++ paper/src/main/java/com/azurhosts/mcquery/McQueryPaperPlugin.java
@@ -1,5 +1,15 @@
 package com.azurhosts.mcquery;
 
+/**
+ * MCQuery - Classe principale du plugin avec compatibilité Paper
+ * Mainteneur : RedSavant, OxiWan (contact@azurhosts.com)
+ * Auteur(s)   : RedSavant (rscomeback@outlook.fr)
+ * Distribué sous GNU General Public License v3.0
+ * Voir LICENSE, CONTRIBUTING.md pour plus de détails.
+ * NOTICE (GPL v3 Section 7b) : L'attribution au mainteneur doit être conservée dans toute redistribution.
+ *
+ **/
+
 public class McQueryPaperPlugin extends Bootstrap{
 
     @Override
