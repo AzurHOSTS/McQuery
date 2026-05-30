dee8bf049d08d18a44011f9b607d266217b6e0be
RedSavant <jjumper999@gmail.com>
2026-05-30 15:17:59 +0200

    feat(add): add server infos in query instructions
    - add `GET_SERVER_INFO` instruction
    - add index per world

--- common/src/main/java/com/azurhosts/mcquery/query/UDPServer.java
+++ common/src/main/java/com/azurhosts/mcquery/query/UDPServer.java
@@ -1,24 +1,25 @@
 package com.azurhosts.mcquery.query;
 
+import com.azurhosts.mcquery.Bootstrap;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.Statistic;
+import org.bukkit.*;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.scoreboard.Team;
 
 import javax.annotation.Nullable;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 /**
  * MCQuery - Classe de gestion des instruction et packets de la query
@@ -63,130 +64,209 @@ public class UDPServer implements Runnable {
     }
 
     private String handleInstruction(String instruction) {
-        if (instruction.equals("GET_PLAYERS")) {
-            JsonObject root = new JsonObject();
-            root.addProperty("type", "GET_PLAYERS");
-            root.addProperty("timestamp", System.currentTimeMillis());
-            root.addProperty("server_tps", Math.min(20.0, Bukkit.getTPS()[0]));
+        switch (instruction) {
+            /* Players Infos */
+            case "GET_PLAYERS" -> {
+                JsonObject root = new JsonObject();
+                root.addProperty("type", "GET_PLAYERS");
+                root.addProperty("timestamp", System.currentTimeMillis());
+                JsonArray players = new JsonArray();
 
-            JsonArray players = new JsonArray();
+                for (Player p : Bukkit.getOnlinePlayers()) {
+                    JsonObject player = new JsonObject();
 
-            for (Player p : Bukkit.getOnlinePlayers()) {
-                JsonObject player = new JsonObject();
+                    player.addProperty("name", p.getName());
+                    player.addProperty("display_name", PlainTextComponentSerializer.plainText().serialize(p.displayName()));
+                    player.addProperty("uuid", p.getUniqueId().toString());
+                    player.addProperty("ip", p.getAddress() != null ? p.getAddress().getAddress().getHostAddress() : null);
+                    player.addProperty("ping", p.getPing());
+                    player.addProperty("locale", p.locale().toString());
+                    player.addProperty("client_brand", p.getClientBrandName());
 
-                player.addProperty("name", p.getName());
-                player.addProperty("display_name", PlainTextComponentSerializer.plainText().serialize(p.displayName()));
-                player.addProperty("uuid", p.getUniqueId().toString());
-                player.addProperty("ip", p.getAddress() != null ? p.getAddress().getAddress().getHostAddress() : null);
-                player.addProperty("ping", p.getPing());
-                player.addProperty("locale", p.locale().toString());
-                player.addProperty("client_brand", p.getClientBrandName());
+                    player.addProperty("gamemode", p.getGameMode().name());
+                    player.addProperty("is_op", p.isOp());
+                    player.addProperty("is_flying", p.isFlying());
+                    player.addProperty("allow_flight", p.getAllowFlight());
+                    player.addProperty("is_sneaking", p.isSneaking());
+                    player.addProperty("is_sprinting", p.isSprinting());
+                    player.addProperty("is_sleeping", p.isSleeping());
+                    player.addProperty("is_blocked", p.isSleepingIgnored());
+                    player.addProperty("is_dead", p.isDead());
 
-                player.addProperty("gamemode", p.getGameMode().name());
-                player.addProperty("is_op", p.isOp());
-                player.addProperty("is_flying", p.isFlying());
-                player.addProperty("allow_flight", p.getAllowFlight());
-                player.addProperty("is_sneaking", p.isSneaking());
-                player.addProperty("is_sprinting", p.isSprinting());
-                player.addProperty("is_sleeping", p.isSleeping());
-                player.addProperty("is_blocked", p.isSleepingIgnored());
-                player.addProperty("is_dead", p.isDead());
+                    player.addProperty("health", p.getHealth());
+                    player.addProperty("max_health", p.getAttribute(Attribute.MAX_HEALTH).getValue());
+                    player.addProperty("absorption", p.getAbsorptionAmount());
+                    player.addProperty("food_level", p.getFoodLevel());
+                    player.addProperty("saturation", p.getSaturation());
+                    player.addProperty("exhaustion", p.getExhaustion());
+                    player.addProperty("air", p.getRemainingAir());
+                    player.addProperty("max_air", p.getMaximumAir());
+                    player.addProperty("fire_ticks", p.getFireTicks());
+                    player.addProperty("freeze_ticks", p.getFreezeTicks());
+                    player.addProperty("arrow_count", p.getArrowsInBody());
+                    player.addProperty("exp", p.getExp());
+                    player.addProperty("level", p.getLevel());
+                    player.addProperty("total_exp", p.getTotalExperience());
 
-                player.addProperty("health", p.getHealth());
-                player.addProperty("max_health", p.getAttribute(Attribute.MAX_HEALTH).getValue());
-                player.addProperty("absorption", p.getAbsorptionAmount());
-                player.addProperty("food_level", p.getFoodLevel());
-                player.addProperty("saturation", p.getSaturation());
-                player.addProperty("exhaustion", p.getExhaustion());
-                player.addProperty("air", p.getRemainingAir());
-                player.addProperty("max_air", p.getMaximumAir());
-                player.addProperty("fire_ticks", p.getFireTicks());
-                player.addProperty("freeze_ticks", p.getFreezeTicks());
-                player.addProperty("arrow_count", p.getArrowsInBody());
-                player.addProperty("exp", p.getExp());
-                player.addProperty("level", p.getLevel());
-                player.addProperty("total_exp", p.getTotalExperience());
+                    Location loc = p.getLocation();
+                    JsonObject pos = new JsonObject();
+                    pos.addProperty("world", loc.getWorld().getName());
+                    pos.addProperty("x", loc.getX());
+                    pos.addProperty("y", loc.getY());
+                    pos.addProperty("z", loc.getZ());
+                    pos.addProperty("yaw", loc.getYaw());
+                    pos.addProperty("pitch", loc.getPitch());
+                    pos.addProperty("biome", loc.getBlock().getBiome().toString());
+                    pos.addProperty("light_level", loc.getBlock().getLightLevel());
+                    player.add("pos", pos);
 
-                Location loc = p.getLocation();
-                JsonObject pos = new JsonObject();
-                pos.addProperty("world", loc.getWorld().getName());
-                pos.addProperty("x", loc.getX());
-                pos.addProperty("y", loc.getY());
-                pos.addProperty("z", loc.getZ());
-                pos.addProperty("yaw", loc.getYaw());
-                pos.addProperty("pitch", loc.getPitch());
-                pos.addProperty("biome", loc.getBlock().getBiome().toString());
-                pos.addProperty("light_level", loc.getBlock().getLightLevel());
-                player.add("pos", pos);
+                    JsonObject inventory = new JsonObject();
+                    inventory.add("mainhand", serializeItem(p.getInventory().getItemInMainHand()));
+                    inventory.add("offhand", serializeItem(p.getInventory().getItemInOffHand()));
 
-                JsonObject inventory = new JsonObject();
+                    JsonObject armor = new JsonObject();
+                    armor.add("helmet", serializeItem(p.getInventory().getHelmet()));
+                    armor.add("chestplate", serializeItem(p.getInventory().getChestplate()));
+                    armor.add("leggings", serializeItem(p.getInventory().getLeggings()));
+                    armor.add("boots", serializeItem(p.getInventory().getBoots()));
+                    inventory.add("armor", armor);
 
-                inventory.add("mainhand", serializeItem(p.getInventory().getItemInMainHand()));
-                inventory.add("offhand", serializeItem(p.getInventory().getItemInOffHand()));
+                    JsonArray contents = new JsonArray();
+                    for (int i = 0; i < 36; i++) {
+                        JsonObject slot = serializeItem(p.getInventory().getItem(i));
+                        slot.addProperty("slot", i);
+                        contents.add(slot);
+                    }
+                    inventory.add("contents", contents);
+                    inventory.addProperty("held_slot", p.getInventory().getHeldItemSlot());
+                    player.add("inventory", inventory);
 
-                JsonObject armor = new JsonObject();
-                armor.add("helmet", serializeItem(p.getInventory().getHelmet()));
-                armor.add("chestplate", serializeItem(p.getInventory().getChestplate()));
-                armor.add("leggings", serializeItem(p.getInventory().getLeggings()));
-                armor.add("boots", serializeItem(p.getInventory().getBoots()));
-                inventory.add("armor", armor);
+                    JsonArray enderchest = new JsonArray();
+                    for (int i = 0; i < p.getEnderChest().getSize(); i++) {
+                        JsonObject slot = serializeItem(p.getEnderChest().getItem(i));
+                        slot.addProperty("slot", i);
+                        enderchest.add(slot);
+                    }
+                    player.add("enderchest", enderchest);
 
-                JsonArray contents = new JsonArray();
-                for (int i = 0; i < 36; i++) {
-                    JsonObject slot = serializeItem(p.getInventory().getItem(i));
-                    slot.addProperty("slot", i);
-                    contents.add(slot);
+                    JsonArray effects = new JsonArray();
+                    for (PotionEffect effect : p.getActivePotionEffects()) {
+                        JsonObject e = new JsonObject();
+                        e.addProperty("type", effect.getType().getKey().getKey());
+                        e.addProperty("amplifier", effect.getAmplifier());
+                        e.addProperty("duration_ticks", effect.getDuration());
+                        e.addProperty("ambient", effect.isAmbient());
+                        e.addProperty("particles", effect.hasParticles());
+                        effects.add(e);
+                    }
+                    player.add("potion_effects", effects);
+
+                    JsonObject session = new JsonObject();
+                    session.addProperty("first_played", p.getFirstPlayed());
+                    session.addProperty("last_played", p.getLastLogin());
+                    session.addProperty("play_time_ticks", p.getStatistic(Statistic.PLAY_ONE_MINUTE));
+                    session.addProperty("deaths", p.getStatistic(Statistic.DEATHS));
+                    session.addProperty("player_kills", p.getStatistic(Statistic.PLAYER_KILLS));
+                    session.addProperty("mob_kills", p.getStatistic(Statistic.MOB_KILLS));
+                    session.addProperty("damage_dealt", p.getStatistic(Statistic.DAMAGE_DEALT));
+                    session.addProperty("damage_taken", p.getStatistic(Statistic.DAMAGE_TAKEN));
+                    session.addProperty("walked_cm", p.getStatistic(Statistic.WALK_ONE_CM));
+                    player.add("stats", session);
+
+                    if (p.getScoreboard().getEntryTeam(p.getName()) != null) {
+                        Team team = p.getScoreboard().getEntryTeam(p.getName());
+                        player.addProperty("team", team.getName());
+                    }
+
+                    players.add(player);
                 }
-                inventory.add("contents", contents);
-                inventory.addProperty("held_slot", p.getInventory().getHeldItemSlot());
 
-                player.add("inventory", inventory);
-
-                JsonArray enderchest = new JsonArray();
-                for (int i = 0; i < p.getEnderChest().getSize(); i++) {
-                    JsonObject slot = serializeItem(p.getEnderChest().getItem(i));
-                    slot.addProperty("slot", i);
-                    enderchest.add(slot);
-                }
-                player.add("enderchest", enderchest);
-
-                JsonArray effects = new JsonArray();
-                for (PotionEffect effect : p.getActivePotionEffects()) {
-                    JsonObject e = new JsonObject();
-                    e.addProperty("type", effect.getType().getKey().getKey());
-                    e.addProperty("amplifier", effect.getAmplifier());
-                    e.addProperty("duration_ticks", effect.getDuration());
-                    e.addProperty("ambient", effect.isAmbient());
-                    e.addProperty("particles", effect.hasParticles());
-                    effects.add(e);
-                }
-                player.add("potion_effects", effects);
-
-                JsonObject session = new JsonObject();
-                session.addProperty("first_played", p.getFirstPlayed());
-                session.addProperty("last_played", p.getLastLogin());
-                session.addProperty("play_time_ticks", p.getStatistic(Statistic.PLAY_ONE_MINUTE));
-                session.addProperty("deaths", p.getStatistic(Statistic.DEATHS));
-                session.addProperty("player_kills", p.getStatistic(Statistic.PLAYER_KILLS));
-                session.addProperty("mob_kills", p.getStatistic(Statistic.MOB_KILLS));
-                session.addProperty("damage_dealt", p.getStatistic(Statistic.DAMAGE_DEALT));
-                session.addProperty("damage_taken", p.getStatistic(Statistic.DAMAGE_TAKEN));
-                session.addProperty("walked_cm", p.getStatistic(Statistic.WALK_ONE_CM));
-                player.add("stats", session);
-
-                if (p.getScoreboard().getEntryTeam(p.getName()) != null) {
-                    Team team = p.getScoreboard().getEntryTeam(p.getName());
-                    player.addProperty("team", team.getName());
-                }
-
-                players.add(player);
+                root.add("players", players);
+                return new Gson().toJson(root);
             }
 
-            root.add("players", players);
-            return new Gson().toJson(root);
-        }
+            /* Servers Infos */
 
-        return "{\"error\":\"unknown_instruction\"}";
+            case "GET_SERVER_INFO" -> {
+                Runtime runtime = Runtime.getRuntime();
+                JsonObject memory = new JsonObject();
+                memory.addProperty("used_mb", (runtime.totalMemory() - runtime.freeMemory()) / 1048576L);
+                memory.addProperty("free_mb", runtime.freeMemory() / 1048576L);
+                memory.addProperty("total_mb", runtime.totalMemory() / 1048576L);
+                memory.addProperty("max_mb", runtime.maxMemory() / 1048576L);
+
+                CompletableFuture<JsonArray> worldsFuture = new CompletableFuture<>();
+                Bukkit.getScheduler().runTask(Bootstrap.getInstance(), () -> {
+                    JsonArray worlds = new JsonArray();
+                    for (World world : Bukkit.getWorlds()) {
+                        JsonObject w = new JsonObject();
+                        w.addProperty("id", Bootstrap.WORLD_INDEX.getOrDefault(world.getName(), -1));
+                        w.addProperty("name", world.getName());
+                        w.addProperty("uuid", world.getUID().toString());
+                        w.addProperty("environment", world.getEnvironment().name());
+                        w.addProperty("difficulty", world.getDifficulty().name());
+                        w.addProperty("seed", world.getSeed());
+                        w.addProperty("time", world.getTime());
+                        w.addProperty("full_time", world.getFullTime());
+                        w.addProperty("is_thundering", world.isThundering());
+                        w.addProperty("has_storm", world.hasStorm());
+                        w.addProperty("player_count", world.getPlayers().size());
+                        w.addProperty("entity_count", world.getEntities().size());
+                        w.addProperty("loaded_chunks", world.getLoadedChunks().length);
+                        w.addProperty("auto_save", world.isAutoSave());
+                        worlds.add(w);
+                    }
+                    worldsFuture.complete(worlds);
+                });
+
+                JsonArray worlds;
+                try {
+                    worlds = worldsFuture.get(3, TimeUnit.SECONDS);
+                } catch (Exception e) {
+                    worlds = new JsonArray();
+                }
+
+                JsonObject root = new JsonObject();
+                root.addProperty("type", "GET_SERVER_INFO");
+                root.addProperty("timestamp", System.currentTimeMillis());
+                root.addProperty("name", Bukkit.getServer().getName());
+                root.addProperty("version", Bukkit.getVersion());
+                root.addProperty("bukkit_version", Bukkit.getBukkitVersion());
+                root.addProperty("minecraft_version", Bukkit.getMinecraftVersion());
+                root.addProperty("motd", PlainTextComponentSerializer.plainText().serialize(Bukkit.motd()));
+                root.addProperty("online_mode", Bukkit.getOnlineMode());
+                root.addProperty("max_players", Bukkit.getMaxPlayers());
+                root.addProperty("player_count", Bukkit.getOnlinePlayers().size());
+                root.addProperty("whitelist_enabled", Bukkit.hasWhitelist());
+                root.addProperty("global_autosave", Bootstrap.checkAutoSave());
+
+                double[] tps = Bukkit.getTPS();
+                JsonObject tpsObj = new JsonObject();
+                tpsObj.addProperty("1m", Math.min(20.0, tps[0]));
+                tpsObj.addProperty("5m", Math.min(20.0, tps[1]));
+                tpsObj.addProperty("15m", Math.min(20.0, tps[2]));
+                root.add("tps", tpsObj);
+
+                root.add("memory", memory);
+                root.add("worlds", worlds);
+
+                JsonArray plugins = new JsonArray();
+                for (Plugin plugin : Bukkit.getPluginManager().getPlugins()) {
+                    JsonObject pl = new JsonObject();
+                    pl.addProperty("name", plugin.getName());
+                    pl.addProperty("version", plugin.getDescription().getVersion());
+                    pl.addProperty("enabled", plugin.isEnabled());
+                    plugins.add(pl);
+                }
+                root.add("plugins", plugins);
+
+                return new Gson().toJson(root);
+            }
+
+            default -> {
+                return "{\"error\":\"unknown_instruction\"}";
+            }
+        }
     }
 
     private JsonObject serializeItem(@Nullable ItemStack item) {
